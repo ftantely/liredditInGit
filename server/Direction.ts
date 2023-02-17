dc20efdb339e2c3788307586e3e05dee0400e6b3
tantely <ftantely@gmail.com>
2023-02-17 12:52:50 -0700

    PackJsonSetUp

---
+++ server/Direction.ts
@@ -0,0 +1,11 @@
+/*
+0_Initialize and Install package JSON
+ npm init -y
+ copy and paste the package JSON
+ npm install
+ yarn install
+ create gitignore
+ npx tsconfig.json
+ go to Javascript and select prettier
+How to undo git: rm -rf .git/
+*/
