45e62c58b2925445319e9d023ccbbbe89d610b11
tantely <ftantely@gmail.com>
2023-02-17 13:30:15 -0700

    1_ApolloServer_Express_SetUp

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,26 @@
-import {MikroORM, RequiredEntityData} from "@mikro-orm/core";
+import {MikroORM} from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 import {Post} from "./entities/Post";
+import express from "express";
+
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    const post = orm.em
-      .fork({})
-      .create(Post, { title: "My first post" } as RequiredEntityData<Post>);
-    await orm.em.persistAndFlush(post);
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    const app = express()
+app.get("/", (_, res)=>{
+    res.send("hello")
+})
+    app.listen(4000, ()=>{
+        console.log("server started on localhost:4000")
+    })
+
+    // const post = orm.em
+    //   .fork({})
+    //   .create(Post, { title: "My second post" } as RequiredEntityData<Post>);
+    // await orm.em.persistAndFlush(post);
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
 };
 
 main().catch((err) => {
