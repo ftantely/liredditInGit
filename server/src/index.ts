500b34d07ca18d81165e89928d18c869d8ec11df
tantely <ftantely@gmail.com>
2023-02-28 13:46:03 -0700

    9_Register

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,36 +4,35 @@ import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import {HelloResolver} from "./resolvers/hello";
-import {PostResolver} from "./resolvers/post";
-;
-
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
-    // console.log(orm.em.);
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  // console.log(orm.em.);
 
-    const app = express();
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
-            validate: false,
-        }),
-        context: () => ({ em: orm.em }),
-    });
-    // console.log(apolloServer.);
-    await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
-    app.listen(4000, () => {
-        console.log("server started on localhost:4000");
-    });
-    // const post = orm.em
-    //   .fork({})
-    //   .create(Post, { title: "My first post 2" } as RequiredEntityData<Post>);
-    // await orm.em.persistAndFlush(post);
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+  // console.log(apolloServer.);
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+  // const post = orm.em
+  //   .fork({})
+  //   .create(Post, { title: "My first post 2" } as RequiredEntityData<Post>);
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
